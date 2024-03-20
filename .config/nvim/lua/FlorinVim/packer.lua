dc97dd5e4620472b3060bffb3b7660c00392be0f
florin <apetreiflorin236@gmail.com>
2024-03-20 21:14:10 +0100

    change the ai little bit

--- .config/nvim/lua/FlorinVim/packer.lua
+++ .config/nvim/lua/FlorinVim/packer.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
 		{
 			"David-Kunz/gen.nvim",
 			opts = {
-				model = "mistral", -- The default model to use.
+				model = "llama2", -- The default model to use.
 				host = "localhost", -- The host running the Ollama service.
 				port = "11434", -- The port on which the Ollama service is listening.
 				display_mode = "float", -- The display mode. Can be "float" or "split".
